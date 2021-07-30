e4f1ec893c65433c9a9b9fad231c26b91bc1a432
huyviet2582000@gmail.com <huyviet2582000@gmail.com>
2021-07-30 17:53:53 +0700

    Add example file

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueMaterial from 'vue-material'
+// import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 import vuetify from './plugins/vuetify'
+import Element from 'element-ui'
 
 Vue.config.productionTip = false
-Vue.use(VueMaterial)
+Vue.use(Element)
 
 new Vue({
   vuetify,
